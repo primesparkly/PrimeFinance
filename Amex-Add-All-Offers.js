b9dc553711b945086ca460e675f41f23bdbe7a49
primesparkly <112186411+primesparkly@users.noreply.github.com>
2022-08-26 10:38:07 -0400

    Update Amex-Add-All-Offers.js

--- Amex-Add-All-Offers.js
+++ Amex-Add-All-Offers.js
@@ -2,8 +2,8 @@
 var offerButtons = Array.from(document.getElementsByClassName("btn btn-sm btn-fluid offer-cta btn-secondary")).filter(btn => btn.title == "Add to Card");
 var index;
 for (index = 0; index < offerButtons.length; ++index) {
-console.log("Clicking offer button");
-offerButtons[index].click();
-// Wait 2seconds to be nice to AMEX servers
-await new Promise(r => setTimeout(r, 2000));
+  console.log("Clicking offer button");
+  offerButtons[index].click();
+  // Wait 2 seconds to be nice to AMEX servers
+  await new Promise(r => setTimeout(r, 2000));
 }
