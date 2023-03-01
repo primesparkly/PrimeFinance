834b2727fde2d96ab61432fec858f56fedb6d16d
primesparkly <112186411+primesparkly@users.noreply.github.com>
2023-03-01 11:19:00 -0500

    Update Amex-Add-All-Offers.js

--- Amex-Add-All-Offers.js
+++ Amex-Add-All-Offers.js
@@ -1,5 +1,5 @@
 // Find all the "Add to Card" buttons on the page
-var offerButtons = Array.from(document.getElementsByClassName("offer-cta axp-offers__global__fluid___15Bk5 css-1duq00z")).filter(btn => btn.title == "Add to Card");
+var offerButtons = Array.from(document.getElementsByClassName("offer-cta axp-offers__global__fluid___15Bk5")).filter(btn => btn.title == "Add to Card");
 var index;
 for (index = 0; index < offerButtons.length; ++index) {
   console.log("Clicking offer button");
