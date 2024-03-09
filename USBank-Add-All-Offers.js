18aaa0a5ede6631a9543180bb880d9a54dbe71bb
primesparkly <112186411+primesparkly@users.noreply.github.com>
2024-03-09 12:42:02 -0500

    Create USBank-Add-All-Offers.js

---
+++ USBank-Add-All-Offers.js
@@ -0,0 +1,11 @@
+// Find all the "Add to Card" buttons on the page
+var offerButtons = Array.from(document.querySelectorAll("span[class='unActiveOffer'][aria-label='Activate']"));
+var index;
+for (index = 0; index < offerButtons.length; ++index) {
+  console.log("Clicking offer button");
+  offerButtons[index].click();
+  await new Promise(r => setTimeout(r, 2000));
+  document.querySelectorAll("button[class='cdlx-btn']")[0].click();
+  // Wait 2 seconds to be nice to AMEX servers
+  await new Promise(r => setTimeout(r, 2000));
+}
