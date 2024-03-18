1332a2a633b8cf155fd19f86a8b55bf8ed92b7f0
primesparkly <112186411+primesparkly@users.noreply.github.com>
2024-03-17 22:37:59 -0400

    Update Chase-Add-All-Offers.js

--- Chase-Add-All-Offers.js
+++ Chase-Add-All-Offers.js
@@ -3,7 +3,7 @@ javascript: a = () => {
     setTimeout(c, Math.random() * 1000 + 300);
 };
 c = () => {
-    btns = [...document.querySelectorAll('.OfferTile_offerTileContainer__4jplu0')].filter(b => b.childNodes[1].childNodes[0].childNodes[0].type !== 'ico_checkmark_filled');
+    btns = [...document.querySelectorAll("div[role='button'][data-testid='commerce-tile']")].filter(b => b.childNodes[1].childNodes[1].childNodes[0].type !== 'ico_checkmark_filled');
     b = btns.pop();
     if (!b) return console.log('added all!');
     b.childNodes[0].click();
