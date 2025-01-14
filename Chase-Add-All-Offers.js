d052d708956a51d1b22601b45dc5efd684bfca1f
primesparkly <112186411+primesparkly@users.noreply.github.com>
2025-01-14 08:13:48 -0500

    increase wait time for chase offer add

--- Chase-Add-All-Offers.js
+++ Chase-Add-All-Offers.js
@@ -1,12 +1,12 @@
 javascript: a = () => {
     window.history.back();
-    setTimeout(c, Math.random() * 1000 + 300);
+    setTimeout(c, Math.random() * 2000 + 1300);
 };
 c = () => {
     btns = [...document.querySelectorAll("div[role='button'][data-testid='commerce-tile']")].filter(b => b.childNodes[1].childNodes[1].childNodes[0].type !== 'ico_checkmark_filled');
     b = btns.pop();
     if (!b) return console.log('added all!');
     b.childNodes[0].click();
-    setTimeout(a, Math.random() * 1000 + 300);
+    setTimeout(a, Math.random() * 2000 + 300);
 };
 c();
